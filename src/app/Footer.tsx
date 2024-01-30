ec5c231c615c0dae9caaf2470894684b3e74bec4
albert <239856559@qq.com>
2024-01-30 12:47:57 +0800

    complete footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,5 +1,20 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export const Footer = ({ className }: { className?: string }) => {
-  return <footer className={cn("flex")}></footer>;
+  return (
+    <footer
+      className={cn(
+        "flex items-center justify-between space-y-2 text-neutral-400 "
+      )}
+    >
+      <p>
+        © 2023 AlbertM —— Built with{" "}
+        <Link href="https://beta.nextjs.org/">Next.js</Link>,{" "}
+        <Link href="https://mdxjs.com/">MDX</Link>,{" "}
+        <Link href="https://tailwindcss.com/">Tailwind</Link> and{" "}
+        <Link href="https://vercel.com/">Vercel</Link>
+      </p>
+    </footer>
+  );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/style/globals.css";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="container mx-auto px-4 flex min-h-screen flex-col bg-neutral-200">
+      <body className="container mx-auto px-4 flex min-h-screen flex-col bg-neutral-200 ">
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
