9ea5e97ee24843267313e0b8d1a1ea76057ace2b
albert <239856559@qq.com>
2024-02-02 18:20:49 +0800

    add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="container mx-auto px-4 flex min-h-screen flex-col bg-neutral-200 ">
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
