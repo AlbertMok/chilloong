aa996b1ad0b71fc279acea65a14ff4730e45a52a
albert <239856559@qq.com>
2024-02-02 19:57:01 +0800

    add store to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,1 +1,23 @@
 "use client";
+
+import { Provider, atom, createStore } from "jotai";
+import { ReactNode } from "react";
+
+const navbarStore = createStore();
+const currentAtom = atom<string | null>(null);
+const Navbar = ({
+  pathName,
+  children,
+}: {
+  pathName: string;
+  children: ReactNode;
+}) => {
+  navbarStore.set(currentAtom, "");
+  return (
+    <Provider>
+      <nav>
+        <ul></ul>
+      </nav>
+    </Provider>
+  );
+};
