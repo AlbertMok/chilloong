f3d7422ad595bad8e73821b40d7b257ccb12f9f5
albert <239856559@qq.com>
2024-04-10 21:16:57 +0800

    add dark text css

--- src/app/IntroSection.tsx
+++ src/app/IntroSection.tsx
@@ -5,7 +5,7 @@ export const IntroSection = ({ className }: { className?: string }) => (
     <h1 className="mb-6 font-serif text-5xl font-bold  ">
       你好, 我是 Albert 👋
     </h1>
-    <section className="font-serif leading-7 font-medium ">
+    <section className="font-serif leading-7 font-medium text-lg">
       <p>欢迎来到我的个人网站。</p>
       <p>我使用 Next.js, Typescript, TailwindCSS 开发网页应用。</p>
       <p>我对 web 开发和设计有着浓厚的兴趣，喜欢探索和使用新的工具。</p>
